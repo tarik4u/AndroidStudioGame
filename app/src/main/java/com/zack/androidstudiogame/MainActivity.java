4dbe07d2a7ca7453827541966fd0d35f4e977f77
tarik4u <zakari.tarik.11@gmail.com>
2022-05-23 19:16:19 +0200

    Setup

---
+++ app/src/main/java/com/zack/androidstudiogame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zack.androidstudiogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
